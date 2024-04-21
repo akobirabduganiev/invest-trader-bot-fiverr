7d10fc46d208a451bd7cf64c5aef6c16f13ab17e
Akobir Abduganiev <akobir.abduganiev@ya.ru>
2024-04-21 23:27:28 +0500

    Initial commit

---
+++ src/main/java/tech/nuqta/investtraderbotfiverr/InvestTraderBotFiverrApplication.java
@@ -0,0 +1,13 @@
+package tech.nuqta.investtraderbotfiverr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestTraderBotFiverrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InvestTraderBotFiverrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tech/nuqta/investtraderbotfiverr/InvestTraderBotFiverrApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.nuqta.investtraderbotfiverr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestTraderBotFiverrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
