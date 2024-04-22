10e25ae6a9212f3ee07c62e6976d97e000e696ee
Akobir Abduganiev <akobir.abduganiev@ya.ru>
2024-04-22 17:46:34 +0500

    Add language selection to start message and update dependencies
    An upgrade is done to the dependencies in build.gradle with the addition of 'javax.xml.bind:jaxb-api:2.3.0'. The database URL in application-dev.yml is switched to localhost. A significant enhancement in the start message handling by adding a language selection option in MessageController.java. A new BeansConfig.java file is created for providing auditor awareness.

---
+++ src/main/java/tech/nuqta/investtraderbotfiverr/BeansConfig.java
@@ -0,0 +1,15 @@
+package tech.nuqta.investtraderbotfiverr;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+
+import java.util.Optional;
+
+@Configuration
+public class BeansConfig {
+    @Bean
+    public AuditorAware<String> auditorAware() {
+        return () -> Optional.ofNullable("Your Auditor");
+    }
+}
